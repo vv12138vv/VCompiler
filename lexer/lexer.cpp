6ca5184074d234e8faec54309f1c318ba6bc0dd5
vv12138vv <jgss987611676@outlook.com>
2024-01-19 09:07:50 +0800

    finish generate_rules

--- lexer/lexer.cpp
+++ lexer/lexer.cpp
@@ -1,34 +1,74 @@
 #include"lexer.hpp"
 
+#include <utility>
+
 //读取生成文件
-vector<string> AutoMachine::read_rules(std::string file_name, std::string label) {
-    try{
-        ifstream rules_file(file_name);
-        if(!rules_file.is_open()){
+list<string> AutoMachine::read_rules(const std::string& rules_file_name, const std::string& label) {
+    list<string> rules_string;
+    try {
+        ifstream rules_file(rules_file_name);
+        if (!rules_file.is_open()) {
             throw runtime_error("Can not open the file!");
         }
-        vector<string> rules_string;
         string line;
-        getline(rules_file,line);
-        while(line!=label){//连续读取直至目标行
-            getline(rules_file,line);
+        getline(rules_file, line);
+        while (line != label) {//连续读取直至目标行
+            getline(rules_file, line);
         }
-        while(rules_file.peek()!=EOF){
-            getline(rules_file,line);
+        while (rules_file.peek() != EOF) {
+            getline(rules_file, line);
+            if (line.length() != 4 && line.length() != 5) {//判断是否符合三型文法
+                throw runtime_error("The format of rules are error");
+            }
             rules_string.emplace_back(line);
-            if(rules_file.peek()=='['){
+            if (rules_file.peek() == '[') {
                 break;
             }
         }
         rules_file.close();
-        return rules_string;
-    }catch(const exception& e){
-        cerr<<e.what()<<'\n';
+    } catch (const exception &e) {
+        cerr << e.what() << '\n';
+    }
+    return rules_string;
+}
+
+AutoMachine::AutoMachine(string rules_file_name, LabelType label_type) : rules_file_name_(std::move(rules_file_name)),
+                                                                         label_type_(label_type) {
+    init_rules();
+}
+
+void AutoMachine::init_rules() {
+    list<string> rules_string=std::move(read_rules(rules_file_name_,label_mp[label_type_]));
+    generate_rules(rules_string);
+#ifdef DEBUG
+    for(const auto& rule: rules_){
+        cout<<rule.first<<':'<<rule.second<<'\n';
+    }
+#endif
+    unordered_set<char> terminals;
+    unordered_set<char> non_terminals;
+
+}
+
+void AutoMachine::generate_rules(const list<string> &rules_string) {
+    try {
+        for (const string &rule_string: rules_string) {
+            size_t index = rule_string.find("->");
+            if (index == string::npos) {
+                throw runtime_error("The format of rules are error");
+            }
+            string left=rule_string.substr(0,index);
+            string right=rule_string.substr(index+2,rule_string.length()-index-2);
+            Rule rule{left,right};
+            rules_.emplace_back(rule);
+        }
+    } catch (const exception &e) {
+        cerr << e.what() << '\n';
     }
 }
 
-AutoMachine::AutoMachine(string rules_file_name) {
 
+int main(int argc, char *argv[]) {
+    AutoMachine autoMachine(argv[1], LabelType::Delimiter);
+    return 0;
 }
-
-
--- lexer/lexer.hpp
+++ lexer/lexer.hpp
@@ -1,23 +1,29 @@
 #include<iostream>
-#include<unordered_map>
 #include<vector>
+#include<list>
 #include<string>
+#include<unordered_map>
+#include<unordered_set>
+
 #include<fstream>
 //using
 using namespace std;
 
+//表示空
+const char Nil='@';
+
 class DFAState {
     //状态记录
-    vector<int> states;
+    vector<int> states_;
     //可以转移到的状态
-    unordered_map<char, int> to;
+    unordered_map<char, int> to_;
 };
-
 
 typedef unordered_map<int, DFAState> DFA;
 typedef unordered_map<int, unordered_map<char, vector<int>>> NFA;
+typedef pair<string,string> Rule;
 
-enum TokenType{
+enum class TokenType {
     Constant,
     Delimiter,
     Operand,
@@ -25,10 +31,43 @@ enum TokenType{
     Identifier
 };
 
-class AutoMachine{
-private:
-    vector<string> rules;
+enum class LabelType {
+    Scientific,
+    Delimiter,
+    Operator,
+    Identifier,
+};
+unordered_map<LabelType, string> label_mp{
+    {LabelType::Delimiter, "[delimiter]"},
+    {LabelType::Scientific,"[scientific]"},
+    {LabelType::Operator,"[operator]"},
+    {LabelType::Identifier,"[identifier]"}
+};
+
+class Token {
 public:
-    vector<string> read_rules(string file_name,string label);
-    AutoMachine(string rules_file_name);
+    size_t line_;
+    TokenType type_;
+    string value_;
+    Token(size_t line, TokenType type, string value) : line_(line), type_(type), value_(std::move(value)) {}
+};
+
+
+class AutoMachine {
+private:
+    string rules_file_name_;
+    LabelType label_type_;
+
+    list<Rule> rules_;
+    vector<char> alphabets_;
+    NFA nfa_;
+    DFA dfa_;
+
+public:
+    static list<string> read_rules(const string& rules_file_name, const string& label);
+
+    void generate_rules(const list<string>& rules_string);
+    void init_rules();
+
+    AutoMachine(string rules_file_name,LabelType label_type);
 };
