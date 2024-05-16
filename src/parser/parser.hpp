4ddbc44fb49962e66f747e84279225ed47bf54b2
vv12138vv <vv12138vv@outlook.com>
2024-05-16 09:43:49 +0800

    finish sql

--- src/parser/parser.hpp
+++ src/parser/parser.hpp
@@ -82,10 +82,10 @@ struct TreeNode {
                 auto t=q.front().first;
                 auto level = q.front().second;
                 q.pop();
-                auto node= agnode(graph,const_cast<char*>((t->content_+" level:"+std::to_string(level)).c_str()),true);
+                auto node= agnode(graph,const_cast<char*>((t->content_+" ["+std::to_string(level)+']').c_str()),true);
                 for(int i=t->kids_.size()-1;i>=0;i-=1){
                     q.push({t->kids_[i],level+1});
-                    auto node1= agnode(graph,const_cast<char*>((t->kids_[i]->content_+" level:"+std::to_string(level+1)).c_str()),true);
+                    auto node1= agnode(graph,const_cast<char*>((t->kids_[i]->content_+" ["+std::to_string(level+1)+']').c_str()),true);
                     agedge(graph,node,node1,nullptr,true);
                 }
             }
